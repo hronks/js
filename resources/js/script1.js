14da19a42dd4c1b2d0068acf1ac0fa4bfe14d765
hronks <harry@ronks.org>
2020-09-19 23:09:47 +0100

    a

--- resources/js/script1.js
+++ resources/js/script1.js
@@ -3,14 +3,15 @@ function myFunction() {
   document.getElementById("demo").innerHTML = "Paragraph changed.";
 }
 
-function gen_HL_buttons(context, func){
+function gen_HL_buttons() {
 
+  var n = document.getElementById("HL").value;
 
-  var btn = document.createElement("BUTTON");
-
-  document.body.append("\p");
-  document.body.append(btn);
-
-
-  alert("function called");
+  for(i = 0; i < n; ++i) {
+    var btn = document.createElement("button");
+    btn.innerHTML = "Hidden layer " + (1 + i);
+    btn.id = "HL" + (1 + i);
+    document.getElementById("hidden_layer_selection").append(btn);
+    document.getElementById("hidden_layer_selection").append(" ");
+  }
 }
